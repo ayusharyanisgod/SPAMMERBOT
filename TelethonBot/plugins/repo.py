49f931dd79f635463645b02309a3fee45d808a24
ayusharyanisgod <84650820+ayusharyanisgod@users.noreply.github.com>
2022-01-06 10:58:18 +0530

    Update repo.py

--- TelethonBot/plugins/repo.py
+++ TelethonBot/plugins/repo.py
@@ -3,8 +3,8 @@ from telethon import events, Button
 
 @ATGK.on(events.NewMessage(incoming=True, pattern="/repo"))
 async def repo(event):
-    await event.reply("нєяє'ѕ τнє яєρο ѕαя",
+    await event.reply("нєяє'ѕ ѕαя",
                     buttons=[
-                        [Button.url("яєρο", url="https://github.com/Javes786/SPAMMERBOT")],
-                        [Button.url("sմթթօɾԵ gяουρ", url="https://t.me/DeSTROYxSupport")]
+                        [Button.url("яєρο", url="https://t.me/AnonymusHu_Bot")],
+                        [Button.url("sմթթօɾԵ gяουρ", url="https://t.me/AnonymusHu_Bot")]
                     ])
